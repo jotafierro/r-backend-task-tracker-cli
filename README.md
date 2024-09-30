889de76c0d8fa5108ad2f38b58fd78d7f027ee9c
Jonathan Fierro <jotafierro.tech@gmail.com>
2024-09-30 11:53:08 -0300

    add first steps in README.md and help option to see the description of the available commands

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # r-backend-task-tracker-cli
 
 This project solves the challenge https://roadmap.sh/projects/task-tracker
+
+> My goal is to do this using as few external libraries as possible.
+
+# Requirements
+
+The node version `^20.x.x`
+
+# Install
+
+Clone the project and run the following:
+
+```
+npm i -g .
+````
+
+This will globally install the `task-tracker-cli` command.
+
+# Use
+
+To run the help
+
+```
+task-tracker-cli help
+```
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { argv } from 'node:process'
 import { getFile, updateFile } from './utils/index.js'
 import { add } from './actions/index.js'
@@ -7,6 +9,10 @@ const [ ,, action, ...restArgv ] = argv
 
 const actions = {
   add: add({ getFile, updateFile, STATUS, restArgv }),
+  help: () => {
+    console.log('Available commands:\n')
+    console.log('- add <description>: Add a new task in file todo.json\n')
+  },
 }
 
 actions[action]()
